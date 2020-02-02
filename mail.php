e3c1958b8f5d68f8d1ef18f527ca4d7404bb145b
Daijiro Yokota <dyokota@macalester.edu>
2020-02-02 15:27:36 -0600

    Revert "again"
    This reverts commit 62eaef43a41edba91e964859b3cba2a7b530c4c5.

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $formcontent="From: $name \n Message: $message";
+  $recipient = "daijiroyokota81@gmail.com";
+  $subject = "Contact Form";
+  $mailheader = "From: $email \r\n";
+
+  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+  echo "Thank You!";
+  header("Location: index.html");
+}
+?>
