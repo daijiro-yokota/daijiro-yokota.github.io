6034c1d31787a3895002921cc9c0c0a6c382a006
Daijiro Yokota <dai0801soccer@gmail.com>
2024-10-14 12:47:52 -0400

    update

--- js/dynamic-sidebar.js
+++ js/dynamic-sidebar.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function() {
             <ul class="sidebar-menu">
                 <li class="sidebar-item"><a href="index.html" class="sidebar-link">Home</a></li>
                 <li class="sidebar-item"><a href="about.html" class="sidebar-link">About</a></li>
-                <li class="sidebar-item"><a href="portfolio.html" class="sidebar-link">Portfolio</a></li>
                 <li class="sidebar-item"><a href="blog.html" class="sidebar-link">Blog</a></li>
                 <li class="sidebar-item"><a href="resource.html" class="sidebar-link">Resource</a></li>
                 <li class="sidebar-item"><a href="contact.html" class="sidebar-link">Contact</a></li>
